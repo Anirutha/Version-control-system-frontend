4537c368024a019ff1bda31f23c742d18c19f7b9
Anirutha <aniruthasivakumar03@gmail.com>
2023-06-29 16:17:43 +0530

    all changes done

---
+++ src/Components/DashBoard.js
@@ -0,0 +1,40 @@
+
+import React, { useState } from "react"
+import Base from "../Base/Base"
+import FileList from "../FileList/FileList"
+import FileUpload from "../FileUpload/FileUpload"
+import DropFileInput from "./drop-file-input/DropFileInput"
+
+
+const Dashboard = () =>{
+    const [files, setFiles] = useState([])
+
+    const removeFile = (filename) => {
+      setFiles(files.filter(file => file.name !== filename))
+    }
+
+    const onFileChange = (files) => {
+      console.log(files);
+  }
+
+
+    return (
+        <Base>
+         <div className="title">Upload file</div>         
+      <FileUpload files={files} setFiles={setFiles}
+        removeFile={removeFile} />
+       <FileList files={files} removeFile={removeFile} />
+       <p>Or</p>
+       <div className="box">
+            <h2 className="header">
+                React drop files input
+            </h2>
+            <DropFileInput
+                onFileChange={(files) => onFileChange(files)}
+            />
+        </div>
+       </Base>
+    )
+}
+
+export default Dashboard
---
+++ src/Components/SignupPage.js
@@ -0,0 +1,69 @@
+import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import Base from "../Base/Base"
+import { Button, TextField, Typography } from "@mui/material";
+
+const SignupPage = () => {
+    const navigate = useNavigate();
+    const [username, setUserName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleLogin = async () => {
+        const userDetails = {
+            username,
+            email,
+            password
+        }
+
+        const response = await fetch(`https://auth-delta-murex.vercel.app/users/signup`, {
+            method: "POST",
+            body: JSON.stringify(userDetails),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const data = await response.json()
+        if (data.token) {
+            setError("")
+            localStorage.setItem("token", data.token)
+            navigate("/")
+        } else {
+            setError(data.message)
+        }
+    }
+
+    return (
+        <Base>
+            <TextField label="username" variant="outlined" fullWidth sx={{ m: 1 }}
+                placeholder="Enter the username"
+                value={username}
+                onChange={(e) => setUserName(e.target.value)}
+                type="email"
+            />
+            <TextField label="email" variant="outlined" fullWidth sx={{ m: 1 }}
+                placeholder="Enter the email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+            />
+            <TextField label="password" variant="outlined" fullWidth sx={{ m: 1 }}
+                placeholder="Enter the password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+                type="submit"
+                variant="contained"
+                onClick={handleLogin}
+            >Signup</Button>
+            {error ?
+                <Typography color={"danger"}>
+                    {error}
+                </Typography> : ""}
+        </Base>
+    );
+}
+export default SignupPage
